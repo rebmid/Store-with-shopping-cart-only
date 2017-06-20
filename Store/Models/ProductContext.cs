8ac65784816996c5c4d96c14ac700f71f7a6a512
rebmid <rebmid@microsoft.com>
2017-06-20 13:08:26 -0700

    New Store

---
+++ Store/Models/ProductContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace Store.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("Store")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
